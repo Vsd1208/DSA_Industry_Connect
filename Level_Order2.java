bdeb4a324ed2b1abac5f4973fcfc0b6812596fbf
Vsd1208 <saidhirajvandanapu@gmail.com>
2026-02-14 15:48:35 +0530

    14-02-2026

--- Level_Order2.java
+++ Level_Order2.java
@@ -1,5 +1,54 @@
 import java.util.*;
 
+// class Level_Order2 {
+//     private class Tree {
+//         int data;
+//         Tree left;
+//         Tree right;
+
+//         Tree(int data) {
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+
+//     private void levelorder(Tree root, int level, List<List<Integer>> ans) {
+//         if (root == null)
+//             return;
+//         if (ans.size() == level) {
+//             ans.add(new ArrayList<>());
+//         }
+//         ans.get(level).add(root.data);
+//         if (level % 2 != 0) {
+//             levelorder(root.left, level + 1, ans);
+//             levelorder(root.right, level + 1, ans);
+//         } else {
+//             levelorder(root.right, level + 1, ans);
+//             levelorder(root.left, level + 1, ans);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Level_Order2 obj = new Level_Order2();
+//         Tree root = obj.new Tree(1);
+//         root.left = obj.new Tree(2);
+//         root.right = obj.new Tree(3);
+//         root.left.left = obj.new Tree(4);
+//         root.left.right = obj.new Tree(5);
+//         root.right.left = obj.new Tree(6);
+//         root.right.right = obj.new Tree(7);
+//         root.left.left.left = obj.new Tree(10);
+//         root.left.left.right = obj.new Tree(11);
+//         root.right.right.left = obj.new Tree(8);
+//         root.right.right.right = obj.new Tree(9);
+//         List<List<Integer>> ans = new ArrayList<>();
+//         obj.levelorder(root, 0, ans);
+//         System.out.println(ans);
+//     }
+// }
+// import java.util.*;
+
 class Level_Order2 {
     private class Tree {
         int data;
@@ -16,17 +65,17 @@ class Level_Order2 {
     private void levelorder(Tree root, int level, List<List<Integer>> ans) {
         if (root == null)
             return;
+
         if (ans.size() == level) {
             ans.add(new ArrayList<>());
         }
-        ans.get(level).add(root.data);
-        if (level % 2 != 0) {
-            levelorder(root.left, level + 1, ans);
-            levelorder(root.right, level + 1, ans);
+        if (level % 2 == 0) {
+            ans.get(level).add(root.data);
         } else {
-            levelorder(root.right, level + 1, ans);
-            levelorder(root.left, level + 1, ans);
+            ans.get(level).add(0, root.data);
         }
+        levelorder(root.left, level + 1, ans);
+        levelorder(root.right, level + 1, ans);
     }
 
     public static void main(String[] args) {
@@ -42,6 +91,7 @@ class Level_Order2 {
         root.left.left.right = obj.new Tree(11);
         root.right.right.left = obj.new Tree(8);
         root.right.right.right = obj.new Tree(9);
+
         List<List<Integer>> ans = new ArrayList<>();
         obj.levelorder(root, 0, ans);
         System.out.println(ans);
